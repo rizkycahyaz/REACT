560fccdec6cd3822833607eb3762346eaeca5637
rizky cahya z <rizkycahyaz22@gmail.com>
2023-10-17 16:56:26 +0700

    soal19

---
+++ src/pages/Mahasiswa.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function Mahasiswa(){
+    return (
+        <Container>
+            <Row>
+                <Col><h1>Data Mahasiswa</h1></Col>
+            </Row>
+        </Container>
+
+    );
+}
+
+export default Mahasiswa;
---
+++ src/pages/jurusan.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function jurusan(){
+    return (
+        <Container>
+            <Row>
+                <Col><h1>Data jurusan</h1></Col>
+            </Row>
+        </Container>
+
+    );
+}
+
+export default jurusan;
