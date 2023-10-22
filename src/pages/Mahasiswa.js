9fbd2be123f3dafc959c7a5bf15230fddeb6f30f
rizky cahya z <rizkycahyaz22@gmail.com>
2023-10-22 21:56:11 +0700

    soal20

--- src/pages/Mahasiswa.js
+++ src/pages/Mahasiswa.js
@@ -1,13 +1,52 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Mahasiswa(){
+function Mahasiswa() {
+    const [mhs, setMhs] = useState ([]);
+        const url = "http://localhost:3000/static/";
+        useEffect (() => {
+        fectData();
+        },[]);
+        const fectData = async () =>{
+        const response1 = await axios.get ('http://localhost:3000/api/mhs');
+        const data1 = await response1.data.data;
+        setMhs (data1);
+    }
     return (
         <Container>
-            <Row>
-                <Col><h1>Data Mahasiswa</h1></Col>
-            </Row>
-        </Container>
-
+      <Row>
+        <Col>
+          <h2>Data Mahasiswa</h2>
+        </Col>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Jurusan</th>
+              <th scope="col">gambar</th>
+              <th scope="col">swa_foto</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mhs.map((mh, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{mh.nama}</td>
+                <td>{mh.jurusan}</td>
+                <td>
+                  <img src={url + mh.gambar} height="100" />
+                </td>
+                <td>
+                  <img src={url + mh.swa_foto} height="100" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Row>
+    </Container>
     );
 }
 
